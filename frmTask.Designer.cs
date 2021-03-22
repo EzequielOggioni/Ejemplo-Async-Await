8bb08447e376511a9f815428fb48c39de81cd1a4
Eze oggi <egos27@hotmail.com>
2021-03-22 16:12:37 -0300

    first commit

---
+++ frmTask.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace awaitasync
+{
+    partial class frmTask
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTareas = new System.Windows.Forms.Button();
+            this.btnSync = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnTareas
+            // 
+            this.btnTareas.Location = new System.Drawing.Point(87, 40);
+            this.btnTareas.Name = "btnTareas";
+            this.btnTareas.Size = new System.Drawing.Size(110, 48);
+            this.btnTareas.TabIndex = 0;
+            this.btnTareas.Text = "Ejecutar Asincronicos";
+            this.btnTareas.UseVisualStyleBackColor = true;
+            this.btnTareas.Click += new System.EventHandler(this.btnTareas_Click);
+            // 
+            // btnSync
+            // 
+            this.btnSync.Location = new System.Drawing.Point(277, 43);
+            this.btnSync.Name = "btnSync";
+            this.btnSync.Size = new System.Drawing.Size(72, 43);
+            this.btnSync.TabIndex = 1;
+            this.btnSync.Text = "Sincronico";
+            this.btnSync.UseVisualStyleBackColor = true;
+            this.btnSync.Click += new System.EventHandler(this.btnSync_Click);
+            // 
+            // frmTask
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(617, 272);
+            this.Controls.Add(this.btnSync);
+            this.Controls.Add(this.btnTareas);
+            this.Name = "frmTask";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTareas;
+        private System.Windows.Forms.Button btnSync;
+    }
+}
+
---
+++ frmTask.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace awaitasync
+{
+    public partial class frmTask : Form
+    {
+        public frmTask()
+        {
+            InitializeComponent();
+        }
+
+
+        /// <summary>
+        /// Crea las llamada a las tarea y las espera para mostrar el resultado
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void btnTareas_Click(object sender, EventArgs e)
+        {
+
+
+            //corre la tarea MetodoConDelayAsync
+            Task<int> tareaDelay = this.MetodoConDelayAsync();
+
+            //corre la tarea CrearHiloNuevoAsync
+            Task<int> TareaHiloNuevo = this.CrearHiloNuevoAsync();
+
+
+            MessageBox.Show("paso las task");
+
+
+            // espera el resultado de las tareas pero en un hilo secundario
+            int resultadoTareaDelay = await tareaDelay;
+            int resultadoHiloNuevo = await TareaHiloNuevo;
+
+            MessageBox.Show(resultadoTareaDelay.ToString());
+            MessageBox.Show(resultadoHiloNuevo.ToString());
+
+        }
+
+
+        public int MetodoConDelay()
+        {
+
+            Task.Delay(2000).Wait();
+            MessageBox.Show("probando delay");
+            return 1;
+
+
+        }
+
+        public async Task<int> MetodoConDelayAsync()
+        {
+
+            //ejecuta la tarea en un hilo secundario
+            await Task.Delay(2000);
+            MessageBox.Show("probando delay");
+            return 1;
+
+
+        }
+
+        public async Task<int> CrearHiloNuevoAsync()
+        {
+            //Crea la tarea pero no la ejecuta
+            Task<int>  tareaInterna = new Task<int>(()=>
+            {
+
+                return MetodoWhile();
+
+            });
+
+
+            //Comieza a correr la tarea
+            tareaInterna.Start();
+
+            //retorna el hilo principal y continua en uno secundario la ejecucion de la tarea
+            return await tareaInterna;
+                
+            
+        }
+
+        public int MetodoWhile()
+        {
+
+
+            double i = 1;
+
+            while (i < 500000000)
+                i++;
+
+            this.Invoke((MethodInvoker)delegate () { MessageBox.Show("probando hilo nuevo"); });
+
+            return 5;
+
+
+        }
+
+        private  void  btnSync_Click(object sender, EventArgs e)
+        {
+
+
+            int resultadoTareaDelay =  this.MetodoConDelay();
+
+
+            int resultadoMetodoWhile  = this.MetodoWhile();
+
+
+            MessageBox.Show("paso las task");
+
+         
+
+            MessageBox.Show(resultadoTareaDelay.ToString());
+            MessageBox.Show(resultadoMetodoWhile.ToString());
+        }
+
+    }
+}
